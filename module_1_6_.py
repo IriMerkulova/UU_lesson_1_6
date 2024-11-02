f6dcc347b03a698fb80e0cbdbbc42f2a104a6558
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-11-02 11:55:50 +0300

    Update module_1_6_.py
    добавление комментариев

--- module_1_6_.py
+++ module_1_6_.py
@@ -1,16 +1,16 @@
 my_dict = {'Kit': 1997, 'Tanya': 2001, 'Valya': 1981}
-print(my_dict)
-print(my_dict['Valya'])
-print(my_dict.get('Ira'))
+print('Словарь my_dickt:', my_dict)
+print('Год рождения Вали:', my_dict['Valya'])
+print('Год рождения Иры:', my_dict.get('Ira'))
 my_dict['Ira'] = 1985
 my_dict['Oleg'] = 1983
-a = my_dict.pop('Kit')
-print(a)
-print(my_dict)
+del_key = my_dict.pop('Kit') # убрали ключ 'Kit'
+print('Удаленная Кит:', del_key)
+print('Новый словарь my_dickt:', my_dict)
 
 my_set = {525, 0, 523, 0, 242, 0, 525}
-print(my_set)
-print(my_set.add(35))
-print(my_set.add(15))
-print(my_set.discard(525))
-print(my_set)
+print('Множество:', my_set)
+my_set.add(35) # добавление 35
+my_set.add(15) # добавление 15
+my_set.discard(525) #удаление без ошибки 525
+print('Модифицированное множество my_set:', my_set)
